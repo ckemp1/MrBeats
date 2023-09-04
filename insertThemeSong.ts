d56486ec4cb0e5949f99a48503a68a7b280517df
Chris Kemp <zeewalnut@gmail.com>
2023-09-03 20:26:08 -0500

    new bot who this

---
+++ insertThemeSong.ts
@@ -0,0 +1,23 @@
+import fs from "fs"
+export const insertThemeSong = (id: string, fileName: string) => {
+  var data = fs.readFileSync("./data/themeSongs.json", "utf-8")
+  var themeSongs = Array.from(JSON.parse(data))
+
+  // Check if Id already exists before adding a new entry
+  var newUser = true
+  for (var i = 0; i < themeSongs.length; i++) {
+    if (themeSongs[i]["id"] === id) {
+      newUser = false
+      themeSongs[i]["fileName"] = fileName
+      break
+    }
+  }
+  console.log(`new user?: ${newUser}`)
+  if (newUser) {
+    console.log("NEW")
+    themeSongs.push({ id: id, fileName: fileName })
+  }
+
+  data = JSON.stringify(themeSongs, null, 4)
+  fs.writeFileSync("./data/themeSongs.json", data)
+}
---
+++ playThemeSong.ts
@@ -0,0 +1,65 @@
+import {
+  AudioPlayerStatus,
+  createAudioPlayer,
+  createAudioResource,
+  DiscordGatewayAdapterCreator,
+  joinVoiceChannel,
+} from "@discordjs/voice"
+import fs from "fs"
+import { userIds } from "./constants"
+import { Client } from "discord.js"
+
+/**
+ * STUFF TO DO
+ * ID each of the new users that want a themesong:
+ *    - When a user requests a valid mp3
+ *      - If new:
+ *        - Create a new User; store under json file to start, and maybe use sqlite in the future
+ *      - Tag the id from the message author's id onto the themesong file name Ex: 123123123-themesong.mp3
+ *      - themesong.ts should point towards the storage (again either using a json file or sqlite) for that user. In this case
+ *          it would be if song.includes(user.id) then use that mp3 file
+ */
+
+export const playThemeSong = (client: Client) => {
+  client.on("voiceStateUpdate", (oldMember, newMember) => {
+    let channel = newMember.channel
+    // only play when a user joins the chat, not when you mute or deafen
+    if (channel !== null && channel.isVoice) {
+      var themeSongs: { id: string; fileName: string }[]
+      var data = fs.readFileSync("./data/themeSongs.json", "utf-8")
+      themeSongs = Array.from(JSON.parse(data))
+      for (const users of themeSongs) {
+        if (
+          users.id === newMember.member.user.id &&
+          oldMember.channelId === null
+        ) {
+          try {
+            const audioPlayer = createAudioPlayer()
+            const resource = createAudioResource(
+              // need npm i ffmpeg-static to read audio files or something
+
+              fs.createReadStream(`./data/mp3s/${users.fileName}`)
+            )
+            const connection = joinVoiceChannel({
+              channelId: newMember.member.voice.channelId,
+              guildId: newMember.member.guild.id,
+              adapterCreator: newMember.member.guild
+                .voiceAdapterCreator as unknown as DiscordGatewayAdapterCreator,
+            })
+
+            audioPlayer.play(resource)
+            connection.subscribe(audioPlayer)
+            audioPlayer.on(AudioPlayerStatus.Idle, () => {
+              connection.disconnect()
+              connection.destroy()
+            })
+          } catch (err) {
+            console.log("themesong broke?")
+            console.log(err)
+          }
+          return
+        }
+      }
+    }
+  })
+}
