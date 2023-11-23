7237737000d1babb2d50e76955ab1c915d1015d4
Chris Kemp <zeewalnut@gmail.com>
2023-11-22 22:11:28 -0600

    actually adds the dang python file

---
+++ normalizeSound.py
@@ -0,0 +1,13 @@
+import sys
+from pydub import AudioSegment
+if len(sys.argv) == 1:
+    print("No argument given!")
+    sys.exit()
+
+max_db = -20
+sound = AudioSegment.from_file(sys.argv[1])
+
+new_db = max_db - sound.max_dBFS
+
+new_sound = sound.apply_gain(new_db)
+new_sound.export("./data/mp3s/" + sys.argv[1], format="mp3")
